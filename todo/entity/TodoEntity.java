bcc391bbf77829b84e9da3acbd9173c56fdf6432
Himanshu202002 <himanshukumr20@gmail.com>
2025-10-23 00:09:32 +0530

    TodoEntity.java

--- todo/entity/TodoEntity.java
+++ todo/entity/TodoEntity.java
@@ -1,8 +1,14 @@
 package todo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "todos")
 public class TodoEntity {
     @Id
@@ -11,34 +17,34 @@ public class TodoEntity {
     private boolean completed;
 
     //Constructor, getters, setters
-    public TodoEntity(){}
-
-    public TodoEntity(String task, boolean completed) {
-        this.task = task;
-        this.completed = completed;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTask() {
-        return task;
-    }
-
-    public void setTask(String task) {
-        this.task = task;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
+//    public TodoEntity(){}
+//
+//    public TodoEntity(String task, boolean completed) {
+//        this.task = task;
+//        this.completed = completed;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getTask() {
+//        return task;
+//    }
+//
+//    public void setTask(String task) {
+//        this.task = task;
+//    }
+//
+//    public boolean isCompleted() {
+//        return completed;
+//    }
+//
+  //   public void setCompleted(boolean completed) {
+    //    this.completed = completed;
+ //   }
 }
